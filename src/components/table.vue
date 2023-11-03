5022cbf60c94c1da2d980c41155916eca51cf5f6
mateenshagagi <mateen.shagagi@gmail.com>
2023-11-03 00:27:40 -0700

    Added Colors and Search

--- src/components/table.vue
+++ src/components/table.vue
@@ -1,43 +1,63 @@
 <template>
-  <v-data-table
-    v-model:items-per-page="itemsPerPage"
-    :headers="headers"
-    :items="data"
-    item-value="name"
-    class="elevation-1 secondary"
-  ></v-data-table>
+  <v-app>
+    <v-card>
+      <v-card-title>
+        Intel Cores
+        <v-spacer></v-spacer>
+        <v-text-field 
+          v-model="search" 
+          append-icon="mdi-magnify" 
+          label="Search" 
+          single-line 
+          hide-details>
+        </v-text-field>
+      </v-card-title>
+
+      <v-data-table 
+        v-model:items-per-page="itemsPerPage" 
+        :headers="headers" 
+        :items="data"
+        class="elevation-1 secondary"
+        :search="search"
+        >
+        <template v-slot:item.Status="{ item }">
+          <v-chip :color="getStyle(item.Status)">{{ item.Status }}</v-chip>
+        </template>
+      </v-data-table>
+    </v-card>
+  </v-app>
 </template>
 
 <script>
 import JSONdata from "../assets/data.json";
 
-  export default {
-
-    mounted(){
-      //process data here
-      console.log("mounted");
-    },
-    
-    data () {
-      console.log("data");
-      return {
-        itemsPerPage: 10,
-        headers: [
-          {
-            title: 'Product',
-            align: 'start',
-            sortable: false,
-            key: 'Product',
-          },
-          { title: 'Status', align: 'end', key: 'Status' },
-          { title: 'Cores', align: 'end', key: 'Cores' },
-          { title: 'Lithography', align: 'end', key: 'Lithography' },
-          { title: 'Threads', align: 'end', key: 'Threads' },
-          { title: 'Base Freq', align: 'end', key: 'Base_Freq' },
-          { title: 'Max Turbo Freq', align: 'end', key: 'Max_Turbo_Freq' },
-        ],
-        data: JSONdata
+export default {
+  data() {
+    return {
+      search: '',
+      itemsPerPage: 100,
+      headers: [
+        { title: 'Product', align: 'start', sortable: false, key: 'Product' },
+        { title: 'Status', align: 'end', key: 'Status' },
+        { title: 'Cores', align: 'end', key: 'Cores' },
+        { title: 'Lithography', align: 'end', key: 'Lithography' },
+        { title: 'Threads', align: 'end', key: 'Threads'},
+        { title: 'Base Freq', align: 'end', key: 'Base_Freq' },
+        { title: 'Max Turbo Freq', align: 'end', key: 'Max_Turbo_Freq' },
+      ],
+      data: JSONdata,
+    };
+  },
+  methods: {
+    getStyle(Status) {
+      switch (Status) {
+        case "Launched": return 'green font-weight-bold';
+        case "Launched (with IPU)": return 'blue font-weight-bold';
+        case "Announced": return 'yellow font-weight-bold';
+        case "Discontinued": return 'red font-weight-bold';
+        default: ''
       }
     },
-  }
+  },
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -1,20 +1,27 @@
 import { createApp } from 'vue';
 import App from './App.vue'
 
-import './assets/main.css'
-
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import { VDataTable } from "vuetify/labs/VDataTable";
+import { VChip } from 'vuetify/lib/components/index.mjs';
+import { VCardTitle } from 'vuetify/lib/components/index.mjs';
+import { VSpacer } from 'vuetify/lib/components/index.mjs';
+import { VTextField } from 'vuetify/lib/components/index.mjs';
+import { VCard } from 'vuetify/lib/components/index.mjs';
+import { VApp } from 'vuetify/lib/components/index.mjs';
 import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
   
   const vuetify =  createVuetify({
     components: {
-      VDataTable
-    },
-    theme: {
-        defaultTheme: 'dark'
+      VDataTable,
+      VChip,
+      VCardTitle,
+      VSpacer,
+      VTextField,
+      VCard,
+      VApp
     },
     icons: {
         iconfont: 'mdi', // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
